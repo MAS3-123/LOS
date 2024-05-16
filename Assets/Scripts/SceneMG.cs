d6c30e08c9c86a11cd24102f5d1f854912d1dbd1
MAS3-123 <xogns7210@naver.com>
2024-05-16 17:59:33 +0900

    메인씬 제작중

---
+++ Assets/Scripts/SceneMG.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneMG : MonoBehaviour
+{
+
+    [SerializeField] private Button loadGameButton;
+    [SerializeField] private Button newGameButton;
+    [SerializeField] private Button endGameButton;
+
+
+    void Start()
+    {
+        ClickButton();
+        loadGameButton.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        //    if () 저장된 데이터가 있다면
+        //    {
+        //        loadGameButton.gameObject.SetActive (true);
+        //    }
+    }
+
+    private void ClickButton()
+    {
+        loadGameButton.onClick.AddListener(() => { LoadGame(); });
+        newGameButton.onClick.AddListener(() => { NewGame(); });
+        endGameButton.onClick.AddListener(() => { EndGame(); });
+    }
+
+    private void LoadGame()
+    {
+
+    }
+
+    private void NewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void EndGame()
+    {
+
+    }
+}
